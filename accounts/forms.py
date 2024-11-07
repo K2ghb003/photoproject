0c4cd4960c75e7e8ae937434e3fc45403975b5c6
K2ghb003 <spr2440003@stu.o-hara.ac.jp>
2024-11-07 10:19:56 +0900

    Commit

---
+++ accounts/forms.py
@@ -0,0 +1,14 @@
+from django.contrib.auth.forms import UserCreationForm
+from .models import CustomUser
+
+class CustomUserCreationForm(UserCreationForm):
+    class Meta:
+        model = CustomUser
+        fields = ('username', 'email', 'password1', 'password2')
+
+
+# class UserCreationForm(forms.ModelForm):
+    
+#     class Meta:
+#         model = User
+#         fields = ("username",)
